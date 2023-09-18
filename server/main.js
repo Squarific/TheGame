a331ea74bbc6f670251831269e5a163b3f9b72c0
Avery Smets <avery@districtthree.be>
2023-09-18 14:05:48 +0200

    Add a way to register a user

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
+const crypto = require ('crypto'); // Add this to generate a random string 
 const cors = require('cors');
 const app = express();
 const port = 3000;
@@ -8,45 +9,34 @@ const port = 3000;
 // Use the cors middleware to allow cross-origin requests
 app.use(cors());
 
+// Add after other middleware
+app.use(bodyParser.urlencoded({ extended: true })); // For parsing form data
+
 // Connect to SQLite database (or create a new one if it doesn't exist)
 const db = new sqlite3.Database('mydatabase.db');
 
 // Create a table for your data if it doesn't exist
 db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS jsonData (message TEXT, timestamp TEXT)');
+  db.run('CREATE TABLE IF NOT EXISTS users (name TEXT, passphrase TEXT)');
 });
 
 // Middleware for parsing JSON request bodies
 app.use(bodyParser.json());
 
-// Define a route that serves JSON data from the database
-app.get('/api/data', (req, res) => {
-  // Query the database to get JSON data
-  db.all('SELECT * FROM jsonData', (err, rows) => {
+app.post('/register', (req, res) => {
+  const { name } = req.body;
+  const passphrase = crypto.randomBytes(16).toString('hex');
+
+  // Insert the user data into users table
+  db.run('INSERT INTO users (name, passphrase) VALUES (?, ?)', [name, passphrase], (err) => {
     if (err) {
-      console.error('Error fetching JSON data', err);
+      console.error('Error registering user', err);
       res.status(500).json({ error: 'Internal Server Error' });
     } else {
-      // Send the JSON response
-      res.json(rows);
+      res.status(201).json({ passphrase: passphrase }); // Return the passphrase as response
     }
   });
 });
-
-// Define a route to add data to the database
-app.post('/api/data', (req, res) => {
-    const { message, timestamp } = req.body;
-  
-    // Insert the data into the database
-    db.run('INSERT INTO jsonData (message, timestamp) VALUES (?, ?)', [message, timestamp], (err) => {
-      if (err) {
-        console.error('Error adding data to the database', err);
-        res.status(500).json({ error: 'Internal Server Error' });
-      } else {
-        res.status(201).json({ message: 'Data added successfully' });
-      }
-    });
-  });
 
 // Start the Express.js server
 app.listen(port, () => {
