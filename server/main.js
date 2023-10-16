930f5b3afbfb12dfbf11d7f5f750090794844492
Avery Smets <avery@districtthree.be>
2023-10-16 17:19:19 +0200

    Add hearts based on time

--- server/main.js
+++ server/main.js
@@ -9,11 +9,11 @@ const port = 3000;
 const db = new sqlite3.Database('mydatabase.db');
 
 db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, passphrase TEXT)');
+  db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, passphrase TEXT, last_collected DATETIME)');
   db.run('CREATE TABLE IF NOT EXISTS abilities (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)');
   db.run('CREATE TABLE IF NOT EXISTS users_abilities (userid INTEGER, abilityid INTEGER, FOREIGN KEY(userid) REFERENCES users(id), FOREIGN KEY(abilityid) REFERENCES abilities(id))');
   db.run('CREATE TABLE IF NOT EXISTS inventory (id INTEGER PRIMARY KEY AUTOINCREMENT, userid INTEGER, item TEXT, quantity INTEGER, FOREIGN KEY(userid) REFERENCES users(id))');
-  
+
   db.run("INSERT INTO abilities (name) SELECT 'CollectLove' WHERE NOT EXISTS(SELECT 1 FROM abilities WHERE name = 'CollectLove')", (err) => {
     if (err) {
       console.error('Error adding ability', err);
@@ -27,7 +27,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-// This endpoint will return a list of available abilities.
 app.get('/abilities', (req, res) => {
   db.all('SELECT * FROM abilities', (err, rows) => {
     if (err) {
@@ -61,7 +60,6 @@ app.post('/register', (req, res) => {
       return;
     }
 
-    // Insert abilities into users_abilities table
     abilities.forEach(ability => {
       db.run('INSERT INTO users_abilities (userid, abilityid) VALUES ((SELECT id FROM users WHERE passphrase = ?), ?)',
         [passphrase, ability], (err) => {
@@ -100,11 +98,32 @@ app.get('/abilities/:passphrase', (req, res) => {
 });
 
 app.post('/collectLove/:passphrase', (req, res) => {
-  db.run('INSERT INTO inventory (userid, item, quantity) VALUES ((SELECT id FROM users WHERE passphrase = ?), "heart", 1)', [req.params.passphrase], (err) => {
+  db.get('SELECT last_collected FROM users WHERE passphrase = ?', [req.params.passphrase], (err, user) => {
     if (err) {
       res.status(500).json({ error: 'Internal Server Error' });
+    } else if (user) {
+      const last_collected = user.last_collected ? new Date(user.last_collected) : null;
+      const now = new Date();
+      const diffMinutes = last_collected ? Math.floor((now - last_collected) / 60000) : null; // Difference in minutes
+      const heartToAdd = diffMinutes ? Math.floor(diffMinutes / 5) : 1; // If difference is null (first collect), add one heart
+
+      db.run('UPDATE users SET last_collected = ? WHERE passphrase = ?', [now, req.params.passphrase], (err) => {
+        if (err) {
+          console.error('Error updating last collected time', err);
+          res.status(500).json({ error: 'Internal Server Error' });
+          return;
+        }
+
+        db.run('INSERT INTO inventory (userid, item, quantity) VALUES ((SELECT id FROM users WHERE passphrase = ?), "heart", ?)', [req.params.passphrase, heartToAdd], (err) => {
+          if (err) {
+            res.status(500).json({ error: 'Internal Server Error' });
+          } else {
+            res.status(201).json({ message: `Added ${heartToAdd} hearts to your inventory` });
+          }
+        });
+      });
     } else {
-      res.status(201).json({ message: 'Heart added to your inventory' });
+      res.status(404).json({ error: 'Player not found' });
     }
   });
 });
